64115f0c505b928573c8fe2ceacd2ba685c8975a
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2023-09-23 13:49:47 +0530

    Time: 48 ms (95.59%), Space: 44.3 MB (13.05%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = {};
+    for(let i=0;i<nums.length;i++){
+        if(map[target-nums[i]]>=0) return [ i, map[target-nums[i]] ];
+        else map[nums[i]] = i
+    }
+};
