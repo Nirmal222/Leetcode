362e839dd2751b78c760215a1bf55aab963ca1d5
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2023-09-25 12:51:56 +0530

    Time: 54 ms (48.42%), Space: 41.7 MB (94.71%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let output = [[1]];
+    for (let i = 0; i < numRows - 1; i++) {
+        let x = output[output.length - 1];
+        let row = [1];
+        for (let j = 1; j < x.length; j++) {
+            row.push(x[j]+x[j-1])
+        }
+        row.push(1);
+        output.push(row);
+    }
+    return output;
+};
