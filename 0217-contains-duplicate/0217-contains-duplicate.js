e8373868b7e258c9ae10b0f929d7bd6145a6331e
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2023-10-06 12:20:27 +0530

    Time: 80 ms (61.52%), Space: 54.2 MB (54.50%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let map = {};
+    for(let i=0;i<nums.length;i++){
+        if(!map[nums[i]]) map[nums[i]] = 1;
+        else return true
+    }
+    return false
+};
