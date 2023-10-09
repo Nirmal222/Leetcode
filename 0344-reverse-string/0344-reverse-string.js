62385a4623622bb62824332a0c3072c44e132988
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2023-10-09 17:16:51 +0530

    Time: 83 ms (35.74%), Space: 48.8 MB (83.53%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var s = ["h","e","l","l","o"];
+
+function reverseString(s){
+    s.reverse()  
+}
