f0c7f71db318778403f70f6401cda463822127be
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2023-10-16 12:48:16 +0530

    Time: 171 ms (10.58%), Space: 91.3 MB (7.05%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let str = "";
+    while(head){
+        str = str+head.val;
+        head = head.next
+    };
+    return str==str.split("").reverse().join("");
+};
