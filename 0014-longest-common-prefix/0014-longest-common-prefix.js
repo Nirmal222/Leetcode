41f7718c356e13e659a746c40a2843c2d7873b5a
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2024-01-19 00:12:50 +0530

    Time: 54 ms (66.40%), Space: 42.1 MB (76.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -6,7 +6,7 @@ var longestCommonPrefix = function(strs) {
     let res = "";
     for(let i=0;i<strs[0].length;i++){
         let char = strs[0][i];
-        if(strs.every(str=>str[i]===char)) res+=char
+        if(strs.every((str)=>str[i]==char)) res = res+char;
         else break
     }
     return res
