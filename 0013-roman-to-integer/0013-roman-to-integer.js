dab31058bf1cd5eb8677278b226e8ae0a3ea0c88
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2024-01-18 23:29:45 +0530

    Time: 113 ms (48.33%), Space: 48 MB (30.23%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let obj = { I:1, V:5, X:10, L:50, C:100, D:500, M:1000 };
+    let result = 0;
+    for(let i=0;i<s.length;i++){
+        if(obj[s[i+1]]>obj[s[i]]){
+            result = result + obj[s[i+1]]-obj[s[i]]
+            i++
+        }else{
+            result = result + obj[s[i]]
+        }
+    }    
+    return result;
+};
