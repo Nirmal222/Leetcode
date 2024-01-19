d23167e579918a28c9648b5bf071b10ebde1b3da
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2024-01-20 00:16:18 +0530

    Time: 58 ms (30.07%), Space: 48.9 MB (5.20%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,15 +4,16 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    let j = 0;
-    for(let i=0;i<haystack.length;i++){
-        if(needle[j] === haystack[i]){
-            j++
-            if(j==needle.length) return i-j+1
-        }else {
-            i=i-j
-            j=0
+    let p1 = 0, p2 = 0;
+    while(p1 < haystack.length){
+        if(haystack[p1]===needle[p2]){
+            if(p2 === needle.length-1) return p1-p2
+            p2++
+        }else{
+            p1 = p1-p2
+            p2 = 0
         }
+        p1++
     }
     return -1
 };
