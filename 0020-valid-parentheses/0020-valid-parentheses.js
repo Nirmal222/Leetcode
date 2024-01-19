c6ac10cf7862535622020f5f9f6078219982ece3
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2024-01-19 21:44:54 +0530

    Time: 55 ms (66.01%), Space: 50.7 MB (5.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -6,10 +6,10 @@ var isValid = function(s) {
     let st = [];
     for(let i=0;i<s.length;i++){
         if(st.length===0) st.push(s[i]);
-        else if(st[st.length-1]=="(" && s[i]==")") st.pop();
-        else if(st[st.length-1]=="{" && s[i]=="}") st.pop();
-        else if(st[st.length-1]=="[" && s[i]=="]") st.pop();
+        else if (s[i]==")" && st[st.length-1]=="(") st.pop();
+        else if (s[i]=="}" && st[st.length-1]=="{") st.pop();
+        else if (s[i]=="]" && st[st.length-1]=="[") st.pop();
         else st.push(s[i])
     }
-    return st.length===0
+    return st.length==0
 };
