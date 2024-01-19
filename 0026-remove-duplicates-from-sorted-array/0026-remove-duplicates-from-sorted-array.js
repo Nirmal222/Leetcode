d888543307e53af3dd7f402a8ca82546d64455ec
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2024-01-19 23:13:28 +0530

    Time: 56 ms (88.66%), Space: 52 MB (5.01%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let i=0;
-    for(let j=1;j<nums.length;j++){
-        if(nums[i]!==nums[j]){
-            i++
-            nums[i] = nums[j]
-        }
-    }
+  let i = 0;
+  for(let j=0;j<nums.length;j++){
+      if(nums[j]!==nums[i]){
+          i++;
+          nums[i]=nums[j]
+      }
+  }
     return i+1
 };
