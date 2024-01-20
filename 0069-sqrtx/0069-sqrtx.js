9a0ed3d15bf2867c9d339c5e9a07e94cc7a945af
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2024-01-20 19:58:10 +0530

    Time: 75 ms (34.90%), Space: 52.9 MB (5.76%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -2,12 +2,20 @@
  * @param {number} x
  * @return {number}
  */
+// 0 1 
+// l r
 var mySqrt = function(x) {
-    let l=0,r=x;
+    let l = 0, r = x;
     while(l<=r){
         let mid = Math.floor((l+r)/2);
-        if(mid*mid<=x && (mid+1)*(mid+1)>x) return mid;
-        else if(mid*mid<x) l = mid+1;
-        else r = mid-1;
+        if(mid*mid>x){
+            r = mid-1
+        }else if(mid*mid<x){
+            if((mid+1)*(mid+1)>x && mid*mid<=x) return mid
+            l = mid+1
+        }else{
+            return Math.floor(mid)
+        }
     }
+    return Math.floor(l)
 };
