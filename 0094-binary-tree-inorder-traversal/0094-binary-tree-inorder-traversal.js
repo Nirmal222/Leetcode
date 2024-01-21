af96919450dc96f1c2648660b3860addd2672945
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2024-01-21 20:10:43 +0530

    Time: 55 ms (37.20%), Space: 49 MB (8.10%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -13,7 +13,6 @@
 var inorderTraversal = function(root) {
     let res = [];
     function bt(root){
-        console.log(root)
         if(!root) return;
         bt(root.left);
         res.push(root.val)
