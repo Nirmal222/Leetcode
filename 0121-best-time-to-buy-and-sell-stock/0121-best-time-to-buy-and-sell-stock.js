734c71c68f1176d9fa8e21ab8b0f8df8b98ff0b8
Nirmal <33796030+Nirmal222@users.noreply.github.com>
2024-01-23 23:57:33 +0530

    Time: 57 ms (98.47%), Space: 59.3 MB (5.12%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,16 +3,12 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let max = 0,l = 0, r = 0;
-    while(r<prices.length){
-        if(l==r)r++;
-        else{
-            if(prices[r]>prices[l]){
-                max = Math.max(max,prices[r]-prices[l])
-                r++
-            }else{
-                l++
-            }
+    let DOB = 0,max = 0;
+    for(let i=0;i<prices.length;i++){
+        if(prices[i]-prices[DOB]<0){
+            DOB = i
+        }else{
+            max = Math.max(prices[i]-prices[DOB], max)
         }
     }
     return max
